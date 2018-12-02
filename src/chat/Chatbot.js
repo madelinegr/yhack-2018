fe2b497892840835c92dd6c919586e80d712ff9d
madelinegr <mgriswold99@gmail.com>
2018-12-02 02:19:38 -0500

    added chat bot

--- src/chat/Chatbot.js
+++ src/chat/Chatbot.js
@@ -9,6 +9,12 @@ export default class ChatBot extends Component {
         return (
             <div>
                 <h1>Chatbot Page</h1>
+                <iframe
+    allow="microphone;"
+    width="350"
+    height="500"
+    src="https://console.dialogflow.com/api-client/demo/embedded/2b5694b6-a421-4d24-9b95-0e62d78d9236">
+</iframe>
             </div>
         );
     }
