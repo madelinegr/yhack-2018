74a4296f00db4a1ce8cd4553fd37802d83629138
madelinegr <mgriswold99@gmail.com>
2018-12-01 19:28:23 -0500

    additions to similairty algorithms

--- src/TextAnalysis.js
+++ src/TextAnalysis.js
@@ -12,40 +12,37 @@ class TextAnalysis extends React.Component {
         isLoaded: false,
         categories: [],
         entities: [],
-        users: [
-            {
-                name: 'madeline',
-                messages: [
-                    {
-                        text: 'the world is full of hearts',
-                        entities: [
-                            {name: 'world',
-                            salience: 0.3},
-                            {name: 'hearts',
-                            salience: 0.1}
-                        ],
-                        categories: []
-                    },
-                    {
-                        text: 'do you hear the people sing?',
-                        entities: [
-                            {name: 'sing',
-                            salience: 0.5},
-                            {name: 'people',
-                            salience: 0.1}
-                        ],
-                        categories: [{name: 'people'}]
-                    }
-                ],
-            }
-        ],
+        users: [],
       };
     }
   
     componentDidMount() {
-        let name = 'history';
-        let text = 'George Herbert Walker Bush (1924-2018), served as the 41st U.S. president from 1989 to 1993. He also was a two-term U.S. vice president under Ronald Reagan, from 1981 to 1989. Bush, a World War II naval aviator and Texas oil industry executive, began his political career in the U.S. House of Representatives in 1967. ';
-      
+        const trainn = ['bba', 'bdsm', 'bba', 'bdsm', 'bba', 'bdsm', 'bba', 'bba', 'bdsm',
+            'hersh', 'tired', 'help', 'so waht', 'dead', 'creeps'];
+        const traint = [
+            'Paul Abrams has got it all, brilliance, kindness, and incredible sass, yall sleeping on him!!',
+            'First years from the south when the sun sets at 4pm everyday. Edit: meme vetting creds to Theo Akpinar',        
+        ]/* 'Gabriel Civita Ramirez - make me the happiest gal around and tell me youll go out with me?',
+            '/Shallow/ reacts only. (But really, how fucking good was that movie?)',
+            'ricky hage is the cutest human being on earth.',
+    
+            'tfw you havent bothered to do a lit review or form a coherent thesis but the papers due in an hour...',
+            'Hey Tara S. at Machado Friday night would you like to get coffee sometime?',
+            'Shoutout to Ahmed Ashour and the creative team for Back of the Throat -- four days isnt enough for this incredible and impactful show. Thank you for making it happen.',
+            'me showing up to class for the first time since week 3 to take the midterm',
+            'All I want for Christmas this year is a boyfriend who has a Hufflepuff’s loyalty and work ethic, a Slytherin’s determination and cleverness, a Gryffindor’s adventurousness and chivalry, and a Ravenclaw’s intelligence and love of learning',
+            "I'm so upset. Idk why. I hate going to school. I got into college but I'm still scared if I fail my finals. I hate my teachers they make me feel like a fucking failure in life. I'm drifting apart from my friends. Seems like nobody really cares …",
+            "I really need advice. There's this guy that I like, and I feel like there are so many non-verbal hints that he likes me too, like when he glances at me (often) and when he lights up when he talks to me. But then he's really shy so he doesn't take …",
+            "You laugh at the way I look. I laugh at the way you look when scared by a spirit. It’s like getting scared of a tiny harmless mouse when a hundred scorpions, a thousand ants and a billion cockroaches surround your decaying corps. You stupid wimp …",
+            "Did you hear the good news? He’s finally dead. George H W Bush Sr. I knew he was going to die before the end of the year. I’m so happy. First his hideous disgusting wife dies, now he’s dead. Who’s up for vandalizing his grave? I’ve been holding in this nut throughout November, and I plan to empty it all inside of his corpse’s hollowed our eyeholes. I can’t wait for his son to die. Then I’ll be truly happy.",
+            "To all the selfish sex pests, the empty eye-less zombies who would fuck anyone just to share their emptiness",
+        
+        */
+
+
+        for(let i = 0; i<traint.length; i++){
+            let text = traint[i];
+            let name = trainn[i];
         const postParameters = {
        "document": {
         "content": text,
@@ -71,7 +68,6 @@ class TextAnalysis extends React.Component {
         .then(res => res.json())
         .then(
           (result) => {
-            console.log(result);
             this.setState({
               isLoaded: true,
               categories: result.categories,
@@ -111,6 +107,7 @@ class TextAnalysis extends React.Component {
             });
           }
         )
+        }
     }
   
     render() {
@@ -119,8 +116,10 @@ class TextAnalysis extends React.Component {
         return <div>Error: {error.message}</div>;
       } else if (!isLoaded) {
         return <div>Loading...</div>;
-      } else {
+      } else if (categories != null && categories.length != 0){
         return <div>Res: {categories[0].name}</div>;
+      } else{
+        return <div>nothing here</div>;
       }
     }
 
@@ -129,7 +128,7 @@ class TextAnalysis extends React.Component {
     categorySimilarity(){
         let categories = this.state.categories;
         //if no category, can't narrow it down, so search other things on all users
-        if(categories.length == 0) return this.state.users;
+        if(categories == null || categories.length == 0) return this.state.users;
         //for now, I am just matching the top category, adding more may help
         let category = categories[0];
         //if no results, contrinue to go levels higher according to /
@@ -139,7 +138,7 @@ class TextAnalysis extends React.Component {
             let commonUsers = [];
             this.state.users.forEach(user => {
                 user.messages.forEach(message => {
-                    if(message.categories.length != 0){
+                    if(message.categories != null && message.categories.length != 0){
                         let userPath = message.categories[0].name.split('/');
                         if(userPath.length >= i){
                             let userCategory = userPath.slice(0, i);
@@ -157,36 +156,104 @@ class TextAnalysis extends React.Component {
         return this.state.users;
     }
 
-    entitySimilarity(possibleUsers){
-       // let users = firebase.database().ref('users');
-       
+    entityThesourous(entity, goBack){
+         let text = entity.name;
+             //fetch('http://words.bighugelabs.com/api/2/eb3bde23bd68b58eca5cea41783ea35e/' + text + '/json')
+        fetch('https://www.dictionaryapi.com/api/v3/references/thesaurus/json/' + text + '?key=e2232eec-db13-48e0-bedd-c4afde262ab3')
+        .then(res => res.json())
+        .then(
+        (result) => {
+            console.log(text + " same as  ");
+            console.log(result);
+            if(result != null){
+                result.forEach(res => {
+                    if(typeof res === 'string' || res instanceof String){
+                        this.state.entities.push(
+                            {
+                            name: res,
+                            salience: entity.salience / 3,
+                        });
+                        console.log(this.state.entities);
+                    } else{
+                        if(res.meta.syns != null){
+                            res.meta.syns[0].forEach(syn => {
+                                this.state.entities.push(
+                                    {
+                                    name: syn,
+                                    salience: entity.salience / 3,
+                                });
+                                console.log(this.state.entities);
+                                
+                            });
+                        }
+                        
+                    }       
+                });
+                console.log("returned");
+                //return this.state.entities;
+                goBack();
+            }
+        
+        },
+        (error) => {}
+        );
+    }
+
+    entityCommon(){
         console.log(possibleUsers);
         let userSimilarities = {};
         possibleUsers.forEach(user => {
             let maxSimilarity = 0;
             user.messages.forEach(message=> {
                 let otherEntitites = message.entities;
-                let a = 0, b = 0;
-                if (otherEntitites.length > this.state.entities.length){
-                    a= otherEntitites; b=this.state.entities;
-                } 
-                else{
-                    a = this.state.entities; b = otherEntitites;
+                if(otherEntitites != null && this.state.entities != null){
+                    let a = [], b = [];
+                    if (otherEntitites.length > this.state.entities.length){
+                        a= otherEntitites; b=this.state.entities;
+                    } 
+                    else{
+                        a = this.state.entities; b = otherEntitites;
+                    }
+                    let msgSimilarity = 0;
+                    a.forEach(entity => {
+                        b.forEach(entity2 => {
+
+                            if(entity.name == entity2.name){
+                                msgSimilarity += entity.salience + entity2.salience
+                            }
+                        })
+                    });
+                    maxSimilarity = Math.max(maxSimilarity, msgSimilarity);
                 }
-                let msgSimilarity = 0;
-                a.forEach(entity => {
-                    b.forEach(entity2 => {
-                        if(entity.name == entity2.name){
-                            msgSimilarity += entity.salience + entity2.salience
-                        }
-                    })
-                });
-                maxSimilarity = Math.max(maxSimilarity, msgSimilarity);
             })
             userSimilarities[user.name] = maxSimilarity;
             
         });
         console.log(userSimilarities);
+    }
+
+    entitySimilarity(possibleUsers){
+       // let users = firebase.database().ref('users');
+                  //TODO: for each entitity 
+       if(this.state.entities != null){
+        // let newEntities = [];
+         let allEntitites = this.state.entities;
+         allEntitites.forEach(entity => {
+             let res = this.entityThesourous(entity);
+             console.log("ret = " + res);
+             if(res != null){
+                this.state.entities = res;
+             }
+        }); 
+        console.log("new = " + this.state.entities);
+       // this.state.entities.push(newEntities);  
+       }
+     
+
+       
+        console.log("done = " + this.state.entities);
+
+       
 
     }
 
